5b51d8e71a1f6cdf4b9d1db88a2c970ad016eb5f
Christo du Toit <christo@dutoit.co.uk>
2024-03-15 10:30:15 +0000

    INFRA: Initial checkin

---
+++ STX.Serialization.Providers.SystemTextJson.Infrastructure.Build/Program.cs
@@ -0,0 +1,18 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using STX.Serialization.Providers.SystemTextJson.Infrastructure.Build.Services;
+
+namespace STX.Serialization.Providers.SystemTextJson.Infrastructure.Build
+{
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var scriptGenerationService = new ScriptGenerationService();
+            scriptGenerationService.GenerateBuildScript();
+        }
+    }
+}
