497dfc32390710c5634f7d1b02e49ad819bcd988
Christo du Toit <christo@dutoit.co.uk>
2024-03-27 14:30:47 +0000

    BROKERS: SystemTextSerializationBroker - Serialize

---
+++ STX.Serialization.Providers.SystemTextJson/Brokers/ISystemTextSerializationBroker.cs
@@ -0,0 +1,18 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace STX.Serialization.Providers.SystemTextJson.Brokers
+{
+    internal interface ISystemTextSerializationBroker
+    {
+        ValueTask SerializeAsync<T>(
+            Stream utf8JsonStream,
+            T @object,
+            CancellationToken cancellationToken = default);
+    }
+}
