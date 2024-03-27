876054351765ebabdb1e702b6a7dcdc0c673c7fd
Christo du Toit <christo@dutoit.co.uk>
2024-03-27 12:12:59 +0000

    BROKERS:  SystemTextSerializationBroker - Serialize

---
+++ STX.Serialization.Providers.SystemTextJson/Brokers/ISystemTextSerializationBroker.cs
@@ -0,0 +1,11 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+namespace STX.Serialization.Providers.SystemTextJson.Brokers
+{
+    internal interface ISystemTextSerializationBroker
+    {
+        string Serialize<T>(T @object);
+    }
+}
---
+++ STX.Serialization.Providers.SystemTextJson/Brokers/SystemTextSerializationBroker.cs
@@ -0,0 +1,21 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System.Text.Json;
+
+namespace STX.Serialization.Providers.SystemTextJson.Brokers
+{
+    internal class SystemTextSerializationBroker : ISystemTextSerializationBroker
+    {
+        private readonly JsonSerializerOptions jsonSerializerOptions;
+
+        public SystemTextSerializationBroker(JsonSerializerOptions jsonSerializerOptions)
+        {
+            this.jsonSerializerOptions = jsonSerializerOptions;
+        }
+
+        public string Serialize<T>(T @object) =>
+            JsonSerializer.Serialize(@object, jsonSerializerOptions);
+    }
+}
