b123dad50be05551bf9d97153c6c0c4ade82bb35
Christo du Toit <christo@dutoit.co.uk>
2024-06-06 22:07:32 +0100

    ShouldDeserializeBytesToObjectAsync -> FAIL

--- STX.Serialization.Providers.SystemTextJson.Tests.Unit/Services/Foundations/Serializations/SerializationServiceTests.Logic.Deserialize.cs
+++ STX.Serialization.Providers.SystemTextJson.Tests.Unit/Services/Foundations/Serializations/SerializationServiceTests.Logic.Deserialize.cs
@@ -43,5 +43,35 @@ namespace STX.Serialization.Providers.SystemTextJson.Tests.Unit.Services.Foundat
 
             systemTextSerializationBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldDeserializeBytesToObjectAsync()
+        {
+            // given
+            CancellationToken cancellationToken = default;
+            string randomJsonString = GetRandomString();
+            byte[] inputBytes = Encoding.UTF8.GetBytes(randomJsonString);
+            Stream inputStream = new MemoryStream(inputBytes);
+            dynamic randomObject = CreateRandomObject();
+            object deserializedObject = randomObject;
+            dynamic expectedResult = deserializedObject;
+
+            systemTextSerializationBrokerMock.Setup(service =>
+                service.DeserializeAsync<dynamic>(It.Is(SameStreamAs(inputStream)), cancellationToken))
+                    .ReturnsAsync(deserializedObject);
+
+            // when
+            dynamic actualResult = await this.serializationService
+                .DeserializeAsync<byte[], dynamic>(inputBytes, cancellationToken);
+
+            // then
+            ((object)actualResult).Should().BeEquivalentTo((object)expectedResult);
+
+            systemTextSerializationBrokerMock.Verify(service =>
+                service.DeserializeAsync<dynamic>(It.Is(SameStreamAs(inputStream)), cancellationToken),
+                    Times.Once);
+
+            systemTextSerializationBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
