5e1eaad4b01c6dea97eeefde0aa107faf3b4597a
Christo du Toit <christo@dutoit.co.uk>
2024-06-07 18:42:12 +0100

    PROVIDERS: Serialize

---
+++ STX.Serialization.Providers.SystemTextJson/Models/Providers/Serializations/SystemTextJson/Exceptions/SystemTextJsonSerializationProviderServiceException.cs
@@ -0,0 +1,16 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using STX.Serialization.Providers.Abstractions.Models.Exceptions;
+using Xeptions;
+
+namespace STX.Serialization.Providers.SystemTextJson.Models.Providers.Serializations.SystemTextJson.Exceptions
+{
+    public class SystemTextJsonSerializationProviderServiceException : Xeption, ISerializationServiceException
+    {
+        public SystemTextJsonSerializationProviderServiceException(string message, Xeption innerException)
+            : base(message, innerException)
+        { }
+    }
+}
